09661d71c3e5a4c33cdcb4536769fc0aecc5cf28
1dena1 <yolavitadena2@gmail.com>
2017-05-26 09:12:46 +0700

    pesan
    activity celana dan kemeja

--- Drawer/app/src/main/java/com/risqi17/drawer/PilihPesanan.java
+++ Drawer/app/src/main/java/com/risqi17/drawer/PilihPesanan.java
@@ -1,14 +1,36 @@
 package com.risqi17.drawer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PilihPesanan extends AppCompatActivity {
+    private Button celana;
+    private Button kemeja;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pilih_pesanan);
         getSupportActionBar().setTitle("Pilih Pesanan");
+
+        celana = (Button)findViewById(R.id.btCelana);
+        kemeja = (Button)findViewById(R.id.btKemeja);
+        celana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Celana.class);
+                startActivity(i);
+            }
+        });
+        kemeja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Kemeja.class);
+                startActivity(i);
+            }
+        });
     }
 }
