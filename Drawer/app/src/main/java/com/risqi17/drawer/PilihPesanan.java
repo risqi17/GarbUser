f300a1237f4cdbe8c6f8622976d65b3a0dd43948
DESKTOP-GV55521\ACER ES1 <viviniaaguss@gmail.com>
2017-05-26 09:08:00 +0700

    pilih pesanan

--- Drawer/app/src/main/java/com/risqi17/drawer/PilihPesanan.java
+++ Drawer/app/src/main/java/com/risqi17/drawer/PilihPesanan.java
@@ -9,5 +9,6 @@ public class PilihPesanan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pilih_pesanan);
+        getSupportActionBar().setTitle("Pilih Pesanan");
     }
 }
